61efe0a23e46a7aaa2aa075009d97eae37e69fd7
Josef Staroba <jstaroba@centene.com>
2019-01-07 09:28:20 -0600

    Get prime factor of 2 working

--- src/primeFactors.spec.ts
+++ src/primeFactors.spec.ts
@@ -2,9 +2,15 @@ describe('Prime Factors', () => {
 
   it('can factor into primes', () => {
     expect(list()).toEqual(of(1));
+    expect(list(2)).toEqual(of(2));
   });
 
-  const of = (n: number): Array<number> => new Array<number>();
+  const of = (n: number): Array<number> => {
+    const factors: Array<number> = new Array<number>();
+    if (n === 2)
+      factors.push(2);
+    return factors;
+  }
 
-  const list = () => [];
+  const list = (...ints: Array<number>): Array<number> => ints;
 });

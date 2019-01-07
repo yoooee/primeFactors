fa7d1eb7f339607c03329eac577f9a97a0db76a0
Josef Staroba <jstaroba@centene.com>
2019-01-07 09:29:56 -0600

    Make code more general

--- src/primeFactors.spec.ts
+++ src/primeFactors.spec.ts
@@ -1,5 +1,7 @@
 describe('Prime Factors', () => {
 
+  const list = (...ints: Array<number>): Array<number> => ints;
+
   it('can factor into primes', () => {
     expect(list()).toEqual(of(1));
     expect(list(2)).toEqual(of(2));
@@ -7,10 +9,9 @@ describe('Prime Factors', () => {
 
   const of = (n: number): Array<number> => {
     const factors: Array<number> = new Array<number>();
-    if (n === 2)
+    if (n > 1)
       factors.push(2);
     return factors;
   }
 
-  const list = (...ints: Array<number>): Array<number> => ints;
 });

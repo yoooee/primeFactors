50e75a1792d78c656e88f6b52901d5015b7bfca5
Josef Staroba <jstaroba@centene.com>
2019-01-07 10:00:20 -0600

    Refactor

--- src/primeFactors.spec.ts
+++ src/primeFactors.spec.ts
@@ -2,10 +2,18 @@ describe('Prime Factors', () => {
 
   const list = (...ints: Array<number>): Array<number> => ints;
 
+  const assertPrimeFactors = (n: number, primeFactors: Array<number>) => {
+    expect(primeFactors).toEqual(of(n));
+  }
+
   it('can factor into primes', () => {
-    expect(list()).toEqual(of(1));
-    expect(list(2)).toEqual(of(2));
-    expect(list(3)).toEqual(of(3));
+
+    let n: number = 1;
+    let primeFactors: Array<number> = list();
+
+    assertPrimeFactors(1, list());
+    assertPrimeFactors(2, list(2));
+    assertPrimeFactors(3, list(3));
   });
 
   const of = (n: number): Array<number> => {

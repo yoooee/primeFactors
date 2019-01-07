a2cda3d4d691aec85bfd6a268bf042d92ef914c3
Josef Staroba <jstaroba@centene.com>
2019-01-07 09:31:15 -0600

    Get prime factor of 3 to work and make code more generic

--- src/primeFactors.spec.ts
+++ src/primeFactors.spec.ts
@@ -5,12 +5,13 @@ describe('Prime Factors', () => {
   it('can factor into primes', () => {
     expect(list()).toEqual(of(1));
     expect(list(2)).toEqual(of(2));
+    expect(list(3)).toEqual(of(3));
   });
 
   const of = (n: number): Array<number> => {
     const factors: Array<number> = new Array<number>();
     if (n > 1)
-      factors.push(2);
+      factors.push(n);
     return factors;
   }
 
